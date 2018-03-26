e11c9445b949ef91179cd6439250550250cd9f73
Jeyaganeshan Jeyatharsini <JJeyatharsini@cambio.se>
2018-03-26 15:03:46 +0530

    Eager and lazy loading

---
+++ src/org/jeya/dto/UserDetails4.java
@@ -0,0 +1,56 @@
+package org.jeya.dto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TABLE_USER_DETAILS4_BY_TABLE")
+public class UserDetails4 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int userId;
+	@ElementCollection(fetch=FetchType.EAGER)
+	@JoinTable(name="USER4_ADDRESS",
+			joinColumns=@JoinColumn(name="USER_ID"))
+	private Collection<Address> listOfAddresses = new ArrayList<>(); //made this as public for lazy loading vs eager loading demonstration purpose only
+	/**
+	 * Earlier it was a hash set. Changed to array list because index supporting collection is needed
+	 * to have primary key column for join table
+	 */
+
+	private String userName;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Collection<Address> getListOfAddresses() {
+		return listOfAddresses;
+	}
+
+	public void setListOfAddresses(Collection<Address> listOfAddresses) {
+		this.listOfAddresses = listOfAddresses;
+	}
+}
--- src/org/jeya/hibernate/HibernateTest.java
+++ src/org/jeya/hibernate/HibernateTest.java
@@ -1,8 +1,11 @@
 package org.jeya.hibernate;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.FetchType;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -12,6 +15,7 @@ import org.jeya.dto.LoginName;
 import org.jeya.dto.UserDetails;
 import org.jeya.dto.UserDetails2;
 import org.jeya.dto.UserDetails3;
+import org.jeya.dto.UserDetails4;
 
 public class HibernateTest {
 	public static void main(String[] args) {
@@ -45,7 +49,6 @@ public class HibernateTest {
 		session = sessionFactory.openSession();
 		session.beginTransaction();
 		userDetails = session.get(UserDetails.class, 1); // primary key
-		session.getTransaction().commit();
 		session.close();
 		System.out.println("User Name : " + userDetails.getUserName());// First user from getter (get value from db and bind to attribute, invoke getter method
 		System.out.println("Description : " + userDetails.getDescription());
@@ -65,16 +68,42 @@ public class HibernateTest {
 		
 		UserDetails3 userDetails31 = new UserDetails3();
 		userDetails31.setUserName("User 31");
-		Set<Address> addressList = new HashSet();
-		addressList.add(new Address("Home Street31", "Home City31", "Home State31", "Home Pincode31"));
-		addressList.add(new Address("Home Street32", "Home City32", "Home State32", "Home Pincode32"));
+		Collection<Address> listOfAddresses = new ArrayList<>();
+		listOfAddresses.add(new Address("Home Street31", "Home City31", "Home State31", "Home Pincode31"));
+		listOfAddresses.add(new Address("Home Street32", "Home City32", "Home State32", "Home Pincode32"));
 
-		userDetails31.setListOfAddresses(addressList);
+		userDetails31.setListOfAddresses(listOfAddresses);
 		
 		session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(userDetails31);
 		session.getTransaction().commit();
 		session.close();
+		
+		UserDetails4 userDetails41 = new UserDetails4();
+		userDetails41.setUserName("User 41");
+		Collection<Address> addressList = new ArrayList<>();
+		addressList.add(new Address("Home Street41", "Home City41", "Home State41", "Home Pincode41"));
+		addressList.add(new Address("Home Street42", "Home City42", "Home State42", "Home Pincode42"));
+
+		userDetails41.setListOfAddresses(addressList);
+		
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(userDetails41);
+		session.getTransaction().commit();
+		session.close();
+		
+		userDetails41 = null;
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		userDetails41 = session.get(UserDetails4.class, 6);
+		// in default case or @ElementCollection(fetch=FetchType.LAZY)
+		//session.close(); // Exception in thread "main" org.hibernate.LazyInitializationException: failed to lazily initialize a collection of role: 
+		//org.jeya.dto.UserDetails4.listOfAddresses, could not initialize proxy - no Session
+		System.out.println(userDetails41.getListOfAddresses().size());
+		session.close();
+		
+		System.out.println(userDetails41.getListOfAddresses().size());
 	}
 }
